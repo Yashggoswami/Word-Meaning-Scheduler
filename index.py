009a03990cfd384bcb7ebc33a3ba703a9d39ed02
Yashggoswami <yashgoswami2000@gmail.com>
2021-10-01 11:34:50 +0530

    adding plyer and schedule modules - writing first notification script

--- index.py
+++ index.py
@@ -1,1 +1,7 @@
-print("index file")
+from plyer import notification
+
+notification.notify(
+    title="aaj ka subh samachar",
+    message ="small changes make big differences",
+    timeout = 10
+)
---
+++ readme.txt
@@ -0,0 +1,4 @@
+Python modules used - 
+
+1. plyer
+2. schedule
